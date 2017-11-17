76ba2004a2f03d4c75841774a640907f15d9b42e
Kabdmenrs <bencs521@gmail.com>
2017-11-17 12:38:00 -0500

    Add files via upload
    Work in progress . . .

---
+++ Scout-master/MainJavaScript.js
@@ -0,0 +1,10 @@
+
+
+var points = [];
+
+var tds() = {
+	var input1 = document.getElementById("tds1").value;
+	var input2 = document.getElementById("tda1").value;
+	console.log(input1 / input2);
+	}
+}
